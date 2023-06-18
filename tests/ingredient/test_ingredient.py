1b68afc60cf34efc5ce393ae67805aae2d436e00
Lucas G <lucasg.devmail93@gmail.com>
2023-06-17 21:04:34 -0300

    fix

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    queijo = Ingredient('queijo mussarela')
+    agua = Ingredient('água')
+    frango = Ingredient('frango')
+
+    assert repr(agua) == "Ingredient('água')"
+    assert repr(frango) != "Ingredient('água')"
+    assert hash(queijo) == hash(Ingredient("queijo mussarela"))
+    assert hash(queijo) != hash(Ingredient("queijo parmesão"))
+    assert agua.restrictions == set()
+    assert Restriction.ANIMAL_MEAT in frango.restrictions
+    assert Restriction.ANIMAL_DERIVED in frango.restrictions
